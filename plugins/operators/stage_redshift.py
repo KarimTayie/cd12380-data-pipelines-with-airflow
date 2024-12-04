ec266fd4bd2a8c27739dca824bd1ad29abb74cb5
Karim Tayie <karimsherif2597@gmail.com>
2024-12-05 00:46:30 +0200

    feat: Updated StageToRedshiftOperator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,46 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
+from airflow.secrets.metastore import MetastoreBackend
 from airflow.utils.decorators import apply_defaults
 
+
 class StageToRedshiftOperator(BaseOperator):
-    ui_color = '#358140'
+    ui_color = "#358140"
 
     @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 *args, **kwargs):
+    def __init__(
+        self,
+        redshift_conn_id="",
+        s3_path="",
+        table_name="",
+        *args,
+        **kwargs,
+    ):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = redshift_conn_id
+        self.s3_path = s3_path
+        self.table_name = table_name
+        self.copy_sql = """
+            COPY {}
+            FROM '{}'
+            ACCESS_KEY_ID '{}'
+            SECRET_ACCESS_KEY '{}'
+            FORMAT AS JSON 'auto'
+            REGION 'us-east-1'
+        """
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        metastoreBackend = MetastoreBackend()
+        aws_connection = metastoreBackend.get_connection("aws_credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.conn_id)
 
-
-
-
-
+        self.log.info("Copying data from S3 to Redshift.")
+        formatted_sql = self.copy_sql.format(
+            self.table_name,
+            self.s3_path,
+            aws_connection.login,
+            aws_connection.password,
+        )
+        redshift_hook.run(formatted_sql)
+        self.log.info("Copied data from S3 to Redshift.")
